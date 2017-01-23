5cfd7d70c3c78933cd2906ceac37dfe0dabbb594
Prof. Snuggles <innelemstra@msn.com>
2017-01-24 00:30:30 +0100

    work on skeleton of script

--- client/pipeline.py
+++ client/pipeline.py
@@ -2,14 +2,22 @@
 #Inne Lemstra and Michiel Merkx
 
 import subproccess
+import hardware_inquire
 
-#make index files of refernce genome
+#gather hardware data
+[infoCPU, infoRAM] = hardware_inquire.getInfo()
+
+#setup timers
+
+#make index files of refernce genome and time
+#make seperate module and import prealign function
 indexInputPath = "./e_coli_mg1655.fasta"
 indexOutputPath = "./index_files/index_ecoli"
 
 comIndex = "bowtie2-build -f {0} {1}".format(indexInputPath, indexOutputPath)
 
-#run aligner
+#run aligner and time
+#make seperate module and import align function
 shortReadspath = "./sra_set.fasta"
 alignOutputPath = "./index_files/index_ecoli"
 
