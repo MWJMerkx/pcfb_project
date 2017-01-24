731200387f638b8ea6283537e10bfe02bf5cf5bc
MWJMerkx <m.w.j.merkx@student.rug.nl>
2017-01-24 15:58:22 +0000

    small adjustments to serverside scripts

--- server/server_query.py
+++ server/server_query.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-# query the database and to return benchmark information on SRA's
+# Inne Lemstra
+# Michiel Merkx
+
+# query the database and return benchmark speeds on selected short read aligners
+# requires instalation of mysqlclient python module
+
+
+# sra name input seperated by commas
 
 import MySQLdb as mysqlclient
 
 
-SearchTable = input('Which table to search:\n').split(",")
+SearchTable = input('Which table to search:\n').lower().split(",")
 #print(SearchTable)
+SearchTable.sort()
+
+## connects to mysql server
+## queries server for data from each table mentioned in the input string.
 
 MyClient = mysqlclient.connect( host = "localhost", user= "root", passwd= "")
 MyCursor = MyClient.cursor()
@@ -26,3 +37,6 @@ for Item in SearchTable:
 	print(SearchTable[Counter], 'benchmark:\n', 'Average proccess time:', Benchmark[0][0], 'seconds\n', 'Average align time:', Benchmark[0][1], 'seconds\n' )
 	
 	Counter += 1
+
+
+
