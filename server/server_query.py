deaed8cf2cb833aa98bd28337fda0142030eda44
MWJMerkx <m.w.j.merkx@student.rug.nl>
2017-01-24 15:16:58 +0000

    small changes to serverside scripts

--- server/server_query.py
+++ server/server_query.py
@@ -5,7 +5,7 @@
 import MySQLdb as mysqlclient
 
 
-SearchTable = input('Which table to search:').split(",")
+SearchTable = input('Which table to search:\n').split(",")
 #print(SearchTable)
 
 MyClient = mysqlclient.connect( host = "localhost", user= "root", passwd= "")
@@ -18,9 +18,11 @@ MyCursor.execute(UseDatabase)
 Counter = 0
 
 for Item in SearchTable:
-	BenchmarkQuery = """SELECT sra_id, AVG(proccess_time), AVG(align_time) FROM {0};""".format(SearchTable[Counter])
+	#print('\nSearching for:',SearchTable[Counter])
+	print('\n')
+	BenchmarkQuery = """SELECT AVG(proccess_time), AVG(align_time) FROM {0};""".format(SearchTable[Counter])
 	MyCursor.execute(BenchmarkQuery)
 	Benchmark = MyCursor.fetchall()
-	print(Benchmark)
+	print(SearchTable[Counter], 'benchmark:\n', 'Average proccess time:', Benchmark[0][0], 'seconds\n', 'Average align time:', Benchmark[0][1], 'seconds\n' )
 	
 	Counter += 1
