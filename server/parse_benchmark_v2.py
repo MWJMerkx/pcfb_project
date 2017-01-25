5744ccdb18074327984cc92a447e1c08f2fb1a3e
MWJMerkx <m.w.j.merkx@student.rug.nl>
2017-01-25 15:46:48 +0000

    updated server script as well as added proccessed files

--- server/parse_benchmark_v2.py
+++ server/parse_benchmark_v2.py
@@ -12,28 +12,55 @@
 
 import gzip
 import MySQLdb as mysqlclient
+import subprocess
 import re
 
+Qualimap = '~/Documents/qualimap_v2.2.1/qualimap'
 
-FileName = input('File name:\n')
-#InFile = 'soap_out.txt'
-
-InFile = open(FileName, 'r')
+FileName = input('File name:\n').split()
 
 
+InFile = open(FileName[0], 'r')
+
+subprocess.call("samtools sort -o sorted_output.sam {0}".format(FileName[1]),shell=True)
+subprocess.call(Qualimap +" bamqc -bam sorted_output.sam -outdir qualimap_results",shell=True,executable='/bin/bash')
+
+ResultFile = open('./qualimap_results/genome_results.txt', 'r')
+
+Mapped = []
+SearchFile = ResultFile.read()
+
+print(type(SearchFile))
+ReSearch1 = r"number of reads = (\d+,\d*)"
+Search1 = re.search(ReSearch1, SearchFile)
+number_reads = Search1.group(1).replace(',','')
+
+Mapped.append(number_reads)
+
+ReSearch2 = r"number of mapped reads = (\d+,\d*) .*"
+Search2 = re.search(ReSearch2, SearchFile)
+mapped_reads = Search2.group(1).replace(',','')
+
+Mapped.append(mapped_reads)
+
+ReSearch3 = r"mean mapping quality = (\d+.\d*)"
+Search3 = re.search(ReSearch3, SearchFile)
+
+mapped_quality = Search3.group(1)
+
+Mapped.append(mapped_quality)
 
 
-#MyRe = r"((\w+)_.*)"
-#MySearch = InFile
-#MyResult = re.search(MyRe, MySearch)
-
-#DataBase = MyResult.group(2)
-
-#print('Search for database:', DataBase)
+#print(Mapped)
 
 
 ## opens connection to MySQL server
 ## check for existing table in the database, else create a new one
+
+
+
+
+
 
 
 MyClient = mysqlclient.connect( host = "localhost", user= "root", passwd= "")
@@ -46,69 +73,23 @@ MyCursor.execute(UseDatabase)
 for Line in InFile:
 	
 	InputData = Line.split(',')
+	
+	print(InputData[0],InputData[1],InputData[2],InputData[3],Mapped[0],Mapped[1],Mapped[2],InputData[5],InputData[6])
+	
 	InsertData = """INSERT INTO results SET
-	    aligner_ID='{0}',
-	    proccess_time={1},
-	    align_time={2},
-	    cpu='{3}',
-	    ram='{4}';""".format(InputData[0],InputData[1],InputData[2],InputData[4],InputData[5])
+	    SRA_ID='{0}',
+	    aligner_ID='{1}',
+	    proccess_time={2},
+	    align_time={3},
+	    number_reads={4},
+	    mapped_reads={5},
+	    mapped_quality={6},
+	    cpu='{7}',
+	    ram='{8}';""".format(InputData[0],InputData[1],InputData[2],InputData[3],Mapped[0],Mapped[1],Mapped[2],InputData[5],InputData[6])
 	MyCursor.execute(InsertData)
 	MyClient.commit()
-	
-	print(InputData[0],InputData[1],InputData[2],InputData[4],InputData[5])
-	print(InsertData)
-
-
-
-
-
-
-
-#DatabaseCheck = """SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{0}' """.format(DataBase) 
-#
-#DatabaseCheck = """SHOW TABLES LIKE '{0}';""".format(DataBase)
-#
-#MyCursor.execute(DatabaseCheck)
-#
-#DatabaseCheck = str(MyCursor.fetchall())
-#
-#print((DatabaseCheck))
-#
-#if DatabaseCheck.count(DataBase) == 0:
-#	print("Creating new table")
-#	CreateTable = """SET sql_notes = 0;
-#	    CREATE TABLE IF NOT EXISTS {0}(
-#	    ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-#	    sra_id TINYTEXT,
-#	    proccess_time FLOAT,
-#	    align_time FLOAT,
-#	    accuracy FLOAT,
-#	    cpu TINYTEXT,
-#	    ram INT
-#	    );
-#	    SET sql_notes = 1;""".format(DataBase)
-#	MyCursor.execute(CreateTable)
-#
-#else:
-#	print("Appending to existing table")
-
-## instert data into correct table on database
-
-#InsertData = """INSERT INTO results SET
-#    SRA_ID=,
-#    aligner_ID=,
-#    proccess_time=,
-#    align_time=,
-#    mapped_reads=,
-#    mapped_quality=,
-#    cpu=,
-#    ram=,""".format(DataBase)
-
-#MyCursor.execute(InsertData)
-
-
 
 MyCursor.close()
 MyClient.close()
 
-# print ("Data successfully added")
+print ("Data successfully added")
